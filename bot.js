7a915b3026211281bd106aa83b6b6c67bef45fcf
Netflixl <47156778+Netflixl@users.noreply.github.com>
2019-02-28 22:57:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -120,6 +120,32 @@ ${boi2}
 
 
 
+client.on('guildMemberAdd', member => {
+    let channel = member.guild.channels.find('name', 'welcome');
+    let memberavatar = member.user.avatarURL
+      if (!channel) return;
+    let embed = new Discord.RichEmbed()
+        .setColor('#970101')
+        .setThumbnail(memberavatar)
+        .addField(':running_shirt_with_sash: | name :  ',`${member}`)
+        .addField(':loudspeaker: | اطلق من دخل' , `Welcome to the server, ${member}`)
+        .addField(':id: | user :', "**[" + `${member.id}` + "]**" )
+                .addField('➡| انت العضو رقم',`${member.guild.memberCount}`)
+
+                  .addField("Name:",`<@` + `${member.id}` + `>`, true)
+
+                                     .addField(' الـسيرفر', `${member.guild.name}`,true)
+
+     .setFooter(`${member.guild.name}`)
+        .setTimestamp()
+
+      channel.sendEmbed(embed);
+    });
+
+
+
+
+
 client.on('message', message => {
     if (message.content.startsWith('$java')) {
       var rank = "``☆ • Java``";
